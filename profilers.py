11cc3a404221350968fe11530339f48abd4c81aa
Dawid Dutkiewicz <dawid.dutkiewicz@gmail.com>
2020-10-26 12:27:45 +0100

    Add call counter decorator

--- profilers.py
+++ profilers.py
@@ -12,3 +12,19 @@ def timer(func):
         return value
 
     return wrapped
+
+
+CALL_COUNTER = {}
+
+
+def call_counter(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        if func.__name__ not in CALL_COUNTER.keys():
+            CALL_COUNTER[func.__name__] = 1
+        else:
+            CALL_COUNTER[func.__name__] += 1
+        value = func(*args, **kwargs)
+        return value
+
+    return wrapped
